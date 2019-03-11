da9b5445fe8e5a7e3b0a5d0150155ac4508155e2
MarcoFalke <falke.marco@gmail.com>
2019-03-11 16:12:58 -0400

    wallet: Get all balances in one call

--- src/wallet/wallet.h
+++ src/wallet/wallet.h
@@ -992,16 +992,21 @@ public:
     void ProcessModuleMessage(CNode* pfrom, const NetMsgDest& dest, const std::string& strCommand, CDataStream& vRecv) override;
     void ReacceptWalletTransactions(interfaces::Chain::Lock& locked_chain) EXCLUSIVE_LOCKS_REQUIRED(cs_wallet);
     void ResendWalletTransactions(interfaces::Chain::Lock& locked_chain, int64_t nBestBlockTime) override;
-    CAmount GetBalance(const isminefilter& filter=ISMINE_SPENDABLE, const int min_depth=0) const;
-    CAmount GetUnconfirmedBalance() const;
-    CAmount GetImmatureBalance() const;
-    CAmount GetUnconfirmedWatchOnlyBalance() const;
-    CAmount GetImmatureWatchOnlyBalance() const;
-    CAmount GetLegacyBalance(const isminefilter& filter, int minDepth) const;
-    CAmount GetLegacyDenomBalance(std::vector<CAmount>& vecAmounts) const;
+    struct Balance {
+        CAmount m_mine_trusted{0};           //!< Trusted, at depth=GetBalance.min_depth or more
+        CAmount m_mine_untrusted_pending{0}; //!< Untrusted, but in mempool (pending)
+        CAmount m_mine_immature{0};          //!< Immature coinbases in the main chain
+        CAmount m_watchonly_trusted{0};
+        CAmount m_watchonly_untrusted_pending{0};
+        CAmount m_watchonly_immature{0};
+    };
+    Balance GetBalance(int min_depth = 0) const;
+
     CAmount GetAvailableBalance(const CCoinControl* coinControl = nullptr) const;
 
     CAmount GetDenominatedBalance(int nDepth = 0) const;
+    CAmount GetLegacyBalance(const isminefilter& filter, int minDepth) const;
+    CAmount GetLegacyDenomBalance(std::vector<CAmount>& vecAmounts) const;
     float UpdateProgress() const;
 
     bool GetBudgetSystemCollateralTX(interfaces::Chain::Lock& locked_chain, CTransactionRef& tx, uint256 hash, CAmount amount);
