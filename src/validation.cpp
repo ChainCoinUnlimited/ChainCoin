35ea05cff2abc18d8330eda2d7450b4ccd1a9b89
Sjors Provoost <sjors@sprovoost.nl>
2019-03-14 11:30:37 +0100

    [build]: check std::system for -[alert|block|wallet]notify
    Platforms such as iOs do not support launching a process
    through system().

--- src/validation.cpp
+++ src/validation.cpp
@@ -1081,6 +1081,7 @@ static CBlockIndex *pindexBestForkTip = nullptr, *pindexBestForkBase = nullptr;
 static void AlertNotify(const std::string& strMessage)
 {
     uiInterface.NotifyAlertChanged();
+#if defined(HAVE_SYSTEM)
     std::string strCmd = gArgs.GetArg("-alertnotify", "");
     if (strCmd.empty()) return;
 
@@ -1094,6 +1095,7 @@ static void AlertNotify(const std::string& strMessage)
 
     std::thread t(runCommand, strCmd);
     t.detach(); // thread runs free
+#endif
 }
 
 static void CheckForkWarningConditions() EXCLUSIVE_LOCKS_REQUIRED(cs_main)
