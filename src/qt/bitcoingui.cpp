de87852be238f6322a5b8448661397f6db351381
Michael Polzer <>
2019-09-10 10:11:14 +0200

    change background image for modaloverlay

--- src/qt/bitcoingui.cpp
+++ src/qt/bitcoingui.cpp
@@ -1229,7 +1229,7 @@ void BitcoinGUI::message(const QString& title, QString message, unsigned int sty
 void BitcoinGUI::resizeEvent(QResizeEvent* evt)
 {
     QPixmap bkgnd(":/images/walletFrame_bg");
-    bkgnd = bkgnd.scaled(this->size(), Qt::IgnoreAspectRatio, Qt::SmoothTransformation);
+    bkgnd = bkgnd.scaled(this->width(), this->height() - statusBar()->height(), Qt::IgnoreAspectRatio, Qt::SmoothTransformation);
 
     QPalette palette;
 
