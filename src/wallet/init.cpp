18cefdd217d71671e2cc97532453b233cab48ec6
John Newbery <john@johnnewbery.com>
2019-10-14 13:39:25 -0400

    [wallet] Remove pruning check for -rescan option
    Prior to this PR, the wallet would not allow the `-rescan` option at
    startup if pruning was enabled. This is unnecessarily restrictive. It
    should be possible to rescan if pruning is enabled, as long as no blocks
    have actually been pruned yet.
    
    Remove the pruning check from WalletInit::ParameterInteraction(). If any
    blocks have been pruned, that will be caught in CreateWalletFromFile().

--- src/wallet/init.cpp
+++ src/wallet/init.cpp
@@ -129,8 +129,6 @@ bool WalletInit::ParameterInteraction() const
 
     if (gArgs.GetBoolArg("-sysperms", false))
         return InitError("-sysperms is not allowed in combination with enabled wallet functionality");
-    if (gArgs.GetArg("-prune", 0) && gArgs.GetBoolArg("-rescan", false))
-        return InitError(_("Rescans are not possible in pruned mode. You will need to use -reindex which will download the whole blockchain again.").translated);
 
 
     int nLiqProvTmp = gArgs.GetArg("-liquidityprovider", DEFAULT_COINJOIN_LIQUIDITY);
