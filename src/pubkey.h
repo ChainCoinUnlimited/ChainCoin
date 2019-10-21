58af7eaef2f8993cf279824cbeadf35223db60a1
Sebastian Falbesoner <sebastian.falbesoner@gmail.com>
2019-10-22 00:07:31 +0200

    refactor: Remove unused CExt{Pub,}Key (de)serialization methods
    The serialization/deserialization methods for the classes CExtKey and
    CExtPubKey were only used in the BIP32 unit tests, where the relevant parts are
    removed as well.

--- src/pubkey.h
+++ src/pubkey.h
@@ -219,32 +219,6 @@ struct CExtPubKey {
     void Encode(unsigned char code[74]) const;
     void Decode(const unsigned char code[74]);
     bool Derive(CExtPubKey& out, unsigned int nChild) const;
-
-    void Serialize(CSizeComputer& s) const
-    {
-        // Optimized implementation for ::GetSerializeSize that avoids copying.
-        s.seek(BIP32_EXTKEY_SIZE + 1); // add one byte for the size (compact int)
-    }
-
-    template <typename Stream>
-    void Serialize(Stream& s) const
-    {
-        unsigned int len = 74;
-        ::WriteCompactSize(s, len);
-        unsigned char code[74];
-        Encode(code);
-        s.write((const char *)&code[0], len);
-    }
-    template <typename Stream>
-    void Unserialize(Stream& s)
-    {
-        unsigned int len = ::ReadCompactSize(s);
-        unsigned char code[74];
-        if (len != 74)
-            throw std::runtime_error("Invalid extended key size\n");
-        s.read((char *)&code[0], len);
-        Decode(code);
-    }
 };
 
 /** Users of this module must hold an ECCVerifyHandle. The constructor and
