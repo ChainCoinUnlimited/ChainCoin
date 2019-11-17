equalishcoin/equalishcoind

util: Make thread names shorter
Thread names at the process level are limited by 15 characters. This
commit ensures that name 'b-httpworker.42' will not be cropped.

src/util/threadnames.cpp
@@ -57,6 +57,6 @@ static void SetInternalName(std::string name) { }
 
 void util::ThreadRename(std::string&& name)
 {
-    SetThreadName(("bitcoin-" + name).c_str());
+    SetThreadName(("b-" + name).c_str());
     SetInternalName(std::move(name));
 }

ChainCoinUnlimited/ChainCoin

src/util/threadnames.cpp
@@ -57,6 +57,6 @@ static void SetInternalName(std::string name) { }
 
 void util::ThreadRename(std::string&& name)
 {
-    SetThreadName(("chaincoin-" + name).c_str());
+    SetThreadName(("c-" + name).c_str());
     SetInternalName(std::move(name));
 }
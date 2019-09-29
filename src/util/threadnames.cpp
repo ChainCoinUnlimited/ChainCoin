6e7ba520bd97eaf10dc892c7d293203f7aa90674
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2019-09-29 21:49:35 +0300

    util: Make thread names shorter
    Thread names at the process level are limited by 15 characters. This
    commit ensures that name 'b-httpworker.42' will not be cropped.

--- src/util/threadnames.cpp
+++ src/util/threadnames.cpp
@@ -57,6 +57,6 @@ static void SetInternalName(std::string name) { }
 
 void util::ThreadRename(std::string&& name)
 {
-    SetThreadName(("chaincoin-" + name).c_str());
+    SetThreadName(("c-" + name).c_str());
     SetInternalName(std::move(name));
 }
